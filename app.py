7fd9a8395e26bcac501b175c1a422445f936ecd7
Guilhem CHALARD PERENNOU <guilhem.chagper@gmail.com>
2025-03-27 20:00:26 +0100

    init the resource controller and the ovsdb manager in the app

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 from app.blueprints.resources import resources_bp
 from app.blueprints.taps import taps_bp
+from app.controllers.resources_controller import ResourceController
+from utils.ovs_utils import OVSDBManager
 
 def create_app():
     app = Flask(__name__)
+    
+    controller = ResourceController()
+    ovs_manager = OVSDBManager()
+    
+    with app.app_context():
+        app.controller = controller
+        app.ovs_manager = ovs_manager
     
     app.register_blueprint(resources_bp, url_prefix="/resources")
     app.register_blueprint(taps_bp, url_prefix="/resources/taps")
