79ba1623727d91b8c9f1c9643d21dbb2539514bc
Guilhem CHALARD PERENNOU <guilhem.chagper@gmail.com>
2025-03-27 20:01:16 +0100

    add the GET endpoint for taps and use better the resource controller with g object

--- app/blueprints/taps.py
+++ app/blueprints/taps.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, request, jsonify
-
-from ..controllers.resources_controller import ResourceController
+from flask import Blueprint, request, jsonify, current_app, g
 
 taps_bp = Blueprint('taps_bp', __name__)
-resources_controller = ResourceController()
+
+@taps_bp.before_app_request
+def before_request():
+    g.resources_controller = current_app.controller
+    g.ovs_manager = current_app.ovs_manager
 
 # @taps_bp.route('/', methods=['GET'])
 # def get_resources():
@@ -13,6 +15,10 @@ resources_controller = ResourceController()
 # def delete_resources():
 #     return resources_controller.delete_resources()
 
+@taps_bp.route('/', methods=['GET'])
+def get_taps():
+    return g.resources_controller.get_taps(g.ovs_manager)
+
 @taps_bp.route('/', methods=['POST'])
 def create_tap():
-    return resources_controller.create_tap()
+    return g.resources_controller.create_tap(g.ovs_manager)
